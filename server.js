4db4680a9ab0480f8abe044443fd8c4f355a9849
soubhagya <sbarick39@gmail.com>
2020-10-26 07:41:24 +0530

    serverfile

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 exports.__esModule = true;
 var express = require("express");
 var cors = require("cors");
-var getac_1 = require("../server/getac");
-var getphone_1 = require("../server/getphone");
+var getac_1 = require("./ac/getac");
+var getphone_1 = require("./phones/getphone");
 var app = express();
 app.use(cors());
 app.use('/ac', getac_1["default"]);
 app.use('/phones', getphone_1["default"]);
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8081;
 app.listen(port, function () {
     console.log("server started");
 });
--- server.ts
+++ server.ts
@@ -1,14 +1,14 @@
 import * as express from 'express'
 import * as mogodb from "mongodb";
 import * as cors from "cors";
-import  acModule from '../server/getac';
-import  PhoneModule from '../server/getphone';
+import  acModule from './ac/getac';
+import  PhoneModule from './phones/getphone';
 
 let app=express();
 app.use(cors());
 app.use('/ac',acModule);
 app.use('/phones',PhoneModule);
-let port= process.env.PORT ||8080
+let port= process.env.PORT ||8081
 app.listen(port,()=>{
     console.log("server started")
 });
